f927c75842de2afc1b6f35aa07c6267f169f9d23
Vitaliy Nikiforenko <v.nikiforenko.96@gmail.com>
2026-09-13 17:44:55 +0300

    Bump ui-kit to v0.4.6, wire toastEmitter to shared util

--- frontend/src/utils/toastEmitter.ts
+++ frontend/src/utils/toastEmitter.ts
@@ -1,19 +1,2 @@
-import { ToastType } from '@/src/components/sharedComponents/Toast/Toast';
-
-export interface ToastEmitParams {
-  message: string;
-  type?: ToastType;
-  duration?: number;
-}
-
-const TOAST_EVENT = 'wr:toast';
-
-export function showToast(params: ToastEmitParams) {
-  window.dispatchEvent(new CustomEvent(TOAST_EVENT, { detail: params }));
-}
-
-export function onToast(cb: (params: ToastEmitParams) => void) {
-  const handler = (e: Event) => cb((e as CustomEvent<ToastEmitParams>).detail);
-  window.addEventListener(TOAST_EVENT, handler);
-  return () => window.removeEventListener(TOAST_EVENT, handler);
-}
+export type { ToastEmitParams } from '@web-revizor/ui-kit/utils/toastEmitter';
+export { showToast, onToast } from '@web-revizor/ui-kit/utils/toastEmitter';
