f751c2e5bd4038249d06da855f1794d5b0e836e6
Vitaliy Nikiforenko <v.nikiforenko.96@gmail.com>
2026-09-13 14:28:06 +0300

    Bump ui-kit to v0.3.4, use the shared default toast close-icon size/position (identical across all projects, fixes h-auto rendering bug)

--- frontend/src/components/sharedComponents/ToastContainer/ToastContainer.tsx
+++ frontend/src/components/sharedComponents/ToastContainer/ToastContainer.tsx
@@ -12,8 +12,6 @@ const ToastContainer: React.FC<Props> = ({ toasts, onRemove }) => (
     toasts={toasts}
     onRemove={onRemove}
     closeIconName={'common/close-toast'}
-    closeIconSize={16}
-    closeButtonClassName='coloredText absolute right-2 top-1/2 -translate-y-1/2'
   />
 );
 
